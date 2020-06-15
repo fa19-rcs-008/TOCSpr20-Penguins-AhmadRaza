9cf660259689740101be687f63f173aaa5acc778
DESKTOP-D952LR3\Chaudhary_ahmed <fa19-rcs-008@student.comsats.edu.pk>
2020-06-15 23:33:37 +0500

    Task completed
    Task completed

---
+++ Assignment_4/Assets/Penguin/Scripts/Fish.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    [Tooltip("The swim speed")]
+    public float fishSpeed;
+
+    private float randomizedSpeed = 0f;
+    private float nextActionTime = -1f;
+    private Vector3 targetPosition;
+
+
+}
